25e630411273801f9372dc37caae6a39bd8174a2
momirror <471488376@qq.com>
2018-08-07 07:00:15 +0800

    auto go to MainActivity from start activity

--- app/src/main/java/com/example/msp/numbergame/StartActivity.java
+++ app/src/main/java/com/example/msp/numbergame/StartActivity.java
@@ -1,8 +1,12 @@
 package com.example.msp.numbergame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StartActivity extends Activity {
 
@@ -10,5 +14,17 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartActivity.this,MainActivity.class));
+                finish();;
+            }
+        };
+
+        timer.schedule(task,2000);
     }
 }
