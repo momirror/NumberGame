a96d4c19af51bce78001ac6fbf7a088fe7515b42
momirror <471488376@qq.com>
2018-08-07 08:19:18 +0800

    add Select Activity and AboutActivity

--- app/src/main/java/com/example/msp/numbergame/MainActivity.java
+++ app/src/main/java/com/example/msp/numbergame/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.msp.numbergame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnPlay(View v) {
+        startActivity(new Intent(MainActivity.this,SelectActivity.class));
+    }
+
+    public void OnAbout(View v) {
+        startActivity(new Intent(MainActivity.this,AboutActivity.class));
     }
 
 
