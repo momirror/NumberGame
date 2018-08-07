c6d47cdc76c97312a1d8870fe875379c7be1b0cf
momirror <471488376@qq.com>
2018-08-07 21:18:03 +0800

    add music resource files, implement playing music funtionality

--- app/src/main/java/com/example/msp/numbergame/MainActivity.java
+++ app/src/main/java/com/example/msp/numbergame/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.msp.numbergame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    static boolean isPlay = true;
+    MediaPlayer mediaPlayer;
+    Button music_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        music_btn = findViewById(R.id.btn_music);
+        PlayMusic();
+    }
+
+    private void PlayMusic() {
+        mediaPlayer = MediaPlayer.create(this,R.raw.main_music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    public void OnMusic(View v) {
+
+        if(isPlay) {
+            if(mediaPlayer != null) {
+                mediaPlayer.stop();
+                music_btn.setBackgroundResource(R.drawable.btn_music2);
+                isPlay = false;
+            }
+        } else {
+            PlayMusic();
+            music_btn.setBackgroundResource(R.drawable.btn_music1);
+            isPlay = true;
+        }
     }
 
     public void OnPlay(View v) {
@@ -20,6 +49,33 @@ public class MainActivity extends Activity {
 
     public void OnAbout(View v) {
         startActivity(new Intent(MainActivity.this,AboutActivity.class));
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+
+    }
+
+    protected void onDestroy() {
+
+        super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        if(isPlay) {
+            PlayMusic();
+        }
     }
 
 
