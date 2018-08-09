f2abf194de9f509b33e2590c2a413852ec497321
momirror <471488376@qq.com>
2018-08-09 21:26:11 +0800

    implement AboutActivity

--- app/src/main/java/com/example/msp/numbergame/AboutActivity.java
+++ app/src/main/java/com/example/msp/numbergame/AboutActivity.java
@@ -1,13 +1,19 @@
 package com.example.msp.numbergame;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public  void  OnBack(View view) {
+        AboutActivity.this.finish();
     }
 }
